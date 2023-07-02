29fd06475225b0470228603f61dd0c1a0684e0d0
anubhav-sl <anubhav@steinnlabs.com>
2023-07-02 20:47:02 +0530

    finished

--- src/services.js
+++ src/services.js
@@ -1,4 +1,4 @@
-const VITE_API_KEY = import.meta.env.VITE_API_KEY || "5V5OEOEN5K98E697"
+const VITE_API_KEY = import.meta.env.VITE_API_KEY
 
 export const fetchStockData = async (symbol) => {
     const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=${symbol}&apikey=${VITE_API_KEY}`)
